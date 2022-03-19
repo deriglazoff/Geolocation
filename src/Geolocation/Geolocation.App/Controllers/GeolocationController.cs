cf2985793e54d620ed941762ec0808fea5395674
Владислав Дериглазов <deriglazov.v@dobrozaim.ru>
2022-03-19 15:01:04 +0300

    *start project

---
+++ src/Geolocation/Geolocation.App/Controllers/GeolocationController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+
+namespace Geolocation.App.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class GeolocationController : ControllerBase
+    {
+
+
+        private readonly ILogger _logger;
+
+        public GeolocationController(ILogger<GeolocationController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult > Get()
+        {
+            _logger.LogInformation("start");
+            return Ok();
+        }
+    }
+}
